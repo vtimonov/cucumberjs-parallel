c64b7e0fd0c50db745f5c7671b7fcd295d6ba18a
Viacheslav Timonov <tsmaggot@gmail.com>
2022-06-21 13:46:15 +0300

    Release 2.0.2 (#21)
    Co-authored-by: Viacheslav Timonov <v.timonov@zaelab.com>

--- test/unit_tests/jsonReport.test.js
+++ test/unit_tests/jsonReport.test.js
@@ -34,10 +34,21 @@ function setDurationToZero(cases) {
     });
 }
 
-const expected = testData.expectedJson;
-const actual = testData.featuresJson;
+function updateErrorMessage(cases) {
+    cases.forEach(tCase => {
+        tCase.steps.forEach(step => {
+            const errorMessage = step.result.error_message;
+            if (errorMessage) {
+                step.result.error_message = errorMessage.substring(0, errorMessage.indexOf('\n'));
+            }
+        });
+    });
+}
 
-let expectedfeature1, expectedfeature2, actualfeature1, actualfeature2;
+const expected = testData.expectedJson;
+
+
+let expectedfeature1, expectedfeature2, expectedFailing;
 
 
 expected.forEach(value => {
@@ -47,18 +58,27 @@ expected.forEach(value => {
     if ('Cucumber Parallel Set Two' === value.name) {
         expectedfeature2 = value;
     }
-});
-
-actual.forEach(value => {
-    if (value.name === 'Cucumber Parallel Set One') {
-        actualfeature1 = value;
-    }
-    if (value.name === 'Cucumber Parallel Set Two') {
-        actualfeature2 = value;
+    if (value.name === 'Cucumber Parallel Failing') {
+        expectedFailing = value;
     }
 });
 
 describe('Check that json report for parallel features is equal standard cucumber report.', () => {
+    const actual = testData.featuresJson;
+    let actualfeature1, actualfeature2, actualFailing;
+
+    actual.forEach(value => {
+        if (value.name === 'Cucumber Parallel Set One') {
+            actualfeature1 = value;
+        }
+        if (value.name === 'Cucumber Parallel Set Two') {
+            actualfeature2 = value;
+        }
+        if (value.name === 'Cucumber Parallel Failing') {
+            actualFailing = value;
+        }
+    });
+
     it('feature 1 main data should be the same', () => {
         const feature1 = Object.assign({}, expectedfeature1);
         delete feature1.elements;
@@ -70,6 +90,13 @@ describe('Check that json report for parallel features is equal standard cucumbe
         const feature1 = Object.assign({}, expectedfeature2);
         delete feature1.elements;
         const feature2 = Object.assign({}, actualfeature2);
+        delete feature2.elements;
+        expect(feature1).to.deep.equalInAnyOrder(feature2);
+    });
+    it('feature failing main data should be the same', () => {
+        const feature1 = Object.assign({}, expectedFailing);
+        delete feature1.elements;
+        const feature2 = Object.assign({}, actualFailing);
         delete feature2.elements;
         expect(feature1).to.deep.equalInAnyOrder(feature2);
     });
@@ -90,6 +117,20 @@ describe('Check that json report for parallel features is equal standard cucumbe
         //Remove duration as it's dynamic value
         setDurationToZero(elements1);
         setDurationToZero(elements2);
+
+        expect(elements1).to.deep.equalInAnyOrder(elements2);
+    });
+
+    it('feature failing test cases data should be the same', () => {
+        const elements1 = Object.assign([], expectedFailing.elements);
+        const elements2 = Object.assign([], actualFailing.elements);
+
+        //Remove duration as it's dynamic value
+        setDurationToZero(elements1);
+        setDurationToZero(elements2);
+        // Update error message to exclude stacktrace
+        updateErrorMessage(elements1);
+        updateErrorMessage(elements2);
 
         expect(elements1).to.deep.equalInAnyOrder(elements2);
     });
@@ -97,6 +138,21 @@ describe('Check that json report for parallel features is equal standard cucumbe
 
 
 describe('Check that json report for parallel scenarios is equal standard cucumber report.', () => {
+    const actual = testData.scenariosJson;
+    let actualfeature1, actualfeature2, actualFailing;
+
+    actual.forEach(value => {
+        if (value.name === 'Cucumber Parallel Set One') {
+            actualfeature1 = value;
+        }
+        if (value.name === 'Cucumber Parallel Set Two') {
+            actualfeature2 = value;
+        }
+        if (value.name === 'Cucumber Parallel Failing') {
+            actualFailing = value;
+        }
+    });
+
     it('feature 1 main data should be the same', () => {
         const feature1 = Object.assign({}, expectedfeature1);
         delete feature1.elements;
@@ -111,24 +167,11 @@ describe('Check that json report for parallel scenarios is equal standard cucumb
         delete feature2.elements;
         expect(feature1).to.deep.equalInAnyOrder(feature2);
     });
-    it('feature 1 test cases data should be the same', () => {
-        const elements1 = Object.assign([], expectedfeature1.elements);
-        const elements2 = Object.assign([], actualfeature1.elements);
-
-        // Remove duration as it's dynamic value
-        setDurationToZero(elements1);
-        setDurationToZero(elements2);
-
-        expect(elements1).to.deep.equalInAnyOrder(elements2);
-    });
-    it('feature 2 test cases data should be the same', () => {
-        const elements1 = Object.assign([], expectedfeature2.elements);
-        const elements2 = Object.assign([], actualfeature2.elements);
-
-        // Remove duration as it's dynamic value
-        setDurationToZero(elements1);
-        setDurationToZero(elements2);
-
-        expect(elements1).to.deep.equalInAnyOrder(elements2);
+    it('feature failing main data should be the same', () => {
+        const feature1 = Object.assign({}, expectedFailing);
+        delete feature1.elements;
+        const feature2 = Object.assign({}, actualFailing);
+        delete feature2.elements;
+        expect(feature1).to.deep.equalInAnyOrder(feature2);
     });
 });
